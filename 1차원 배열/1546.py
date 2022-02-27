8232960411b439c5cb36b808a285920abe3a1183
Seok Ji Woo <seokjw0727@naver.com>
2022-02-27 18:42:42 +0900

    done 1546 :)

--- 1차원 배열/1546.py
+++ 1차원 배열/1546.py
@@ -1,7 +1,11 @@
-how_much = int(input())
-scores = []
+n = int(input())  # 과목 수
+test_list = list(map(int, input().split())) # 과목 점수
+max_score = max(test_list) # 제일 높은 점수
 
-for i in range(how_much):
-    score = int(input())
-    scores.append(score)
-print(f"{scores / how_much}")
+new_list = []
+for score in test_list : # score 를 test_lit(과목 점수) 만큼 반복함
+    new_list.append(score/max_score *100)  # 새로운 점수 생성, 스코어를 최대 점수로 나눈값 .
+test_avg = sum(new_list)/n # new_list 를 n(과목 수) 로 나눔
+print(test_avg)
+
+
